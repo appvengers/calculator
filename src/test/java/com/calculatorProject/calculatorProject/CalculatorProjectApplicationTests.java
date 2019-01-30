46cea838f1e41b377fe599ec14fcdc45bc0abffc
Rein <rdobreg76 qgmail.com>
2019-01-30 14:23:51 -0500

    prueba unitaria del metodo addTwoNumbers

--- src/test/java/com/calculatorProject/calculatorProject/CalculatorProjectApplicationTests.java
+++ src/test/java/com/calculatorProject/calculatorProject/CalculatorProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.calculatorProject.calculatorProject;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,8 @@ public class CalculatorProjectApplicationTests {
 
 	@Test
 	public void addTest(){
-
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(calculator.addTwoNumbers("2", "3"), "5");
 	}
 
 }
