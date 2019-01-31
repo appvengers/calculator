a3814cd33882a85e06423e5ff92f2687e55d22df
Rein <rdobreg76 qgmail.com>
2019-01-31 12:30:14 -0500

    CORS notation

--- src/main/java/com/calculatorProject/calculatorProject/CalculatorProjectApplication.java
+++ src/main/java/com/calculatorProject/calculatorProject/CalculatorProjectApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,6 +38,7 @@ public class CalculatorProjectApplication {
         }
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping(path = "/services/operations/V2")
     public ResponseEntity<CalculatorOperationResponse> arithmeticOperation(@RequestBody String operation) {
 
